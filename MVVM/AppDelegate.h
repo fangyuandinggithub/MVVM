be1f8136edef352da2a35dbbf6b50289bf42cad4
fangyuandinggithub <3026354242@qq.com>
2019-12-16 14:34:26 +0800

    Initial Commit

---
+++ MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by fangyuan ding on 2019/12/16.
+//  Copyright © 2019年 fangyuan ding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
